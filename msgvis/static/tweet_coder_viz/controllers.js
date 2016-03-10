d534939814570a9a2c25f3d96e966708e57ca6d6
nanchenchen <nanchen.chen@gmail.com>
2016-03-10 04:17:07 -0800

    cleaning up highlighting

--- msgvis/static/tweet_coder_viz/controllers.js
+++ msgvis/static/tweet_coder_viz/controllers.js
@@ -492,6 +492,17 @@
                     });
                 }
             }
+            if (window.getSelection) {
+                if (window.getSelection().empty) {  // Chrome
+                    window.getSelection().empty();
+                }
+                else if (window.getSelection().removeAllRanges) {  // Firefox
+                    window.getSelection().removeAllRanges();
+                }
+            }
+            else if (document.selection) {  // IE?
+                document.selection.empty();
+            }
 
         };
 
