90a1b4bb968927ac80db28e4ae938a79576ada38
Jina Suh <jasmart+github@gmail.com>
2016-07-11 22:21:30 -0700

    Split aeonium to two separate pages

--- msgvis/static/aeonium/app.js
+++ msgvis/static/aeonium/app.js
@@ -12,8 +12,27 @@
             'Aeonium.controllers',
             'ngCookies',
             'ngDragDrop',
-            'ngAnimate'
+            'ngAnimate',
+            'ngRoute'
         ]);
+
+    // Routing
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                        templateUrl : 'static/aeonium/pages/home.html',
+                        controller  : 'HomeController'
+                        })
+            .when('/coding', {
+                        templateUrl : 'static/aeonium/pages/coding.html',
+                        controller  : 'CodingController'
+                        })
+            .when('/review', {
+                        templateUrl : 'static/aeonium/pages/review.html',
+                        controller  : 'ReviewController'
+                        })
+            .otherwise({redirectTo: '/'});
+    });
 
     //Fix CSRF
     //http://django-angular.readthedocs.org/en/latest/csrf-protection.html
@@ -22,6 +41,19 @@
         $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
         $http.defaults.headers.put['X-CSRFToken'] = $cookies.csrftoken;
     }]);
+
+    // Testing
+    app.controller('HomeController', function($scope) {
+        $scope.message = 'Hello from HomeController';
+    });
+
+    app.controller('CodingController', function($scope) {
+        $scope.message = 'Hello from CodingController';
+    });
+
+    app.controller('ReviewController', function($scope) {
+        $scope.message = 'Hello from ReviewController';
+    });
 
 
 })();
