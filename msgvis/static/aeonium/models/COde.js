7b8cae6dfb5549d93afcf129efdb8a431aeeb759
Jina Suh <jasmart+github@gmail.com>
2016-07-24 21:25:05 -0700

    Initial Code model

---
+++ msgvis/static/aeonium/models/COde.js
@@ -0,0 +1,107 @@
+(function () {
+    'use strict';
+
+    var module = angular.module('Aeonium.models');
+
+    //A service for code definition
+    module.factory('Aeonium.models.Code', [
+        '$http', 'djangoUrl', '$rootScope',
+        function codeFactory($http, djangoUrl, $rootScope) {
+
+            //class CodeDefinition {
+            //	codeId: number;
+            //	name: string;
+            //	descriptions: CodeDescription[];
+            //}
+            //
+            //class CodeDescription {
+            //	source: string;
+            //	text: string;
+            //	examples: CodeExample[];
+            //}
+            //
+            //class CodeExample {
+            //	code: number;
+            //	source: string;
+            //	text: string;
+            //}
+
+            var Code = function () {
+                var self = this;
+                self.definitionsByCodeName = {}; // Map<string, CodeDefinition>
+                self.codeDefinitions = []; // CodeDefinition[]
+            };
+
+            angular.extend(Code.prototype, {
+                // returns void
+                loadCodeDefinitions: function(){
+                    var self = this;
+
+                    var sources = ["master", "user", "partner"];
+                    var apiUrl = djangoUrl.reverse('definitions');
+
+                    var request = {
+                        params: {
+                            source: sources.join(" ")
+                        }
+                    };
+
+                    // Returned data format
+                    //"code_id": self.id,
+                    //"code_text": self.text,
+                    //"source": source,
+                    //"text": definition.text,
+                    //"examples": definition.examples
+
+                    return $http.get(apiUrl, request)
+                        .success(function (def_sets) {
+                            def_sets.forEach(function(def_set){
+                                def_set.definitions.forEach(function(def){
+                                    if (!self.definitionsByCodeName[def.code_text]){
+                                        self.definitionsByCodeName[def.code_text] = {
+                                            codeId: def.code_id,
+                                            name: def.code_text,
+                                            descriptions: []
+                                        }
+                                    }
+
+                                    self.definitionsByCodeName[def.code_text].descriptions.push({
+                                        source: def.source,
+                                        text: def.text,
+                                        examples: def.examples
+                                    });
+                                });
+                            });
+
+                            self.codeDefinitions = self.definitionsByCodeName.map(function(codeName) { return self.definitionsByCodeName[codeName]; });
+
+                            $rootScope.$broadcast('Code::updated', self.codeDefinitions);
+                        });
+
+                },
+
+                // codeDefinition: CodeDefinition
+                // newDefinitionText: string
+                // returns void
+                updateDefinition: function(codeDefinition, newDefinitionText){
+                    var self = this;
+                    var apiUrl = djangoUrl.reverse('definition', {code_id: codeDefinition.codeId});
+
+                    var request = {
+                        text: newDefinitionText
+                    };
+
+                    return $http.put(apiUrl, request)
+                        .success(function (data) {
+                            console.log("Update definition for ", code);
+                        });
+
+
+                }
+
+            });
+
+            return new Code();
+        }
+    ]);
+})();
--- msgvis/static/aeonium/models/Message.js
+++ msgvis/static/aeonium/models/Message.js
@@ -1,144 +1,112 @@
 (function () {
     'use strict';
 
-    var module = angular.module('Aeonium.services', [
-        'ng.django.urls',
-        'Aeonium.bootstrap',
-        'ngSanitize'
-    ]);
-
-    module.factory('Aeonium.services.Dictionary', [
-        '$http', 'djangoUrl',
-        'Aeonium.bootstrap.dictionary',
-        function datasetFactory($http, djangoUrl, dictionaryId) {
-            var apiUrl = djangoUrl.reverse('dictionary');
-
-            var Dictionary = function () {
-                this.id = dictionaryId
-            };
-
-            var request = {
-                params: {
-                    id: dictionaryId
-                }
-            };
-            $http.get(apiUrl, request)
-                .success(function (data) {
-                    angular.extend(Dictionary.prototype, data);
-                });
-
-            return new Dictionary();
-
-        }
-    ]);
-
-
-
-    //A service for user defined features.
-    module.factory('Aeonium.services.Feature', [
-        '$http', 'djangoUrl',
-        'Aeonium.services.Code',
-        function scriptFactory($http, djangoUrl, Code) {
-
-            var listApiUrl = djangoUrl.reverse('feature_list');
-
-            var Feature = function () {
-                var self = this;
-                self.latest_data = undefined;
-                self.distributions = {};
-            };
-
-            angular.extend(Feature.prototype, {
-                load: function () {
-                    var self = this;
-
-                    var request = {
-                        params: {}
-                    };
-                    return $http.get(listApiUrl, request)
-                        .success(function (data) {
-                            self.latest_data = data;
-                        });
-                },
-                add: function (tokens, origin_message_id) {
-                    var self = this;
-
-                    var request = {
-                        token_list: tokens,
-                        origin: origin_message_id
-                    };
-
-                    return $http.post(listApiUrl, request)
-                        .success(function (data) {
-                            self.latest_data = data;
-                            self.distributions["user"][data.feature_text] = data;
-                        });
-
-                },
-                remove: function (feature) {
-                    var self = this;
-
-                    var request = {
-
-                    };
-
-                    var itemApiUrl = djangoUrl.reverse('feature', {feature_id: feature.feature_id});
-                    return $http.delete(itemApiUrl, request)
-                        .success(function (data) {
-                            // Remove feature from list
-                            delete self.distributions["user"][feature.feature_text];
-                        });
-                },
-                get_distribution: function(source){
-                    var self = this;
-                    var request = {
-                        params: {
-                            feature_source: source || "user"
-                        }
-                    };
-
-                    var apiUrl= djangoUrl.reverse('distribution');
-                    return $http.get(apiUrl, request)
-                        .success(function (data) {
-
-                            self.distributions[source] = data;
-                            // TODO: understand this and make sure the feature is added to the list
-                            // WHAT!?!?!? Array in JS is also object and it is being used in both way in this case...
-                            data.forEach(function(feature){
-                                // Add to the dict
-                                self.distributions[source]["" + feature.feature_text] = feature;
-                            });
-
-                        });
-                }
-            });
-
-            return new Feature();
-        }
-    ]);
-
+    var module = angular.module('Aeonium.models');
 
     //A service for message
-    module.factory('Aeonium.services.Message', [
+    module.factory('Aeonium.models.Message', [
         '$http', 'djangoUrl', '$rootScope',
-        'Aeonium.services.Code',
-        'Aeonium.services.Progress',
-        function messageFactory($http, djangoUrl, $rootScope, Code, Progress) {
+        'Aeonium.models.Code',
+        function messageFactory($http, djangoUrl, $rootScope, Code) {
 
             var Message = function () {
                 var self = this;
-                self.last_message = undefined;
-                self.current_message = undefined;
-                self.all_coded_messages = [];
-                self.coded_messages = {
-                    master: {},
-                    user: {},
-                    partner: {}
-                };
+                self.allMessages = [];
+                self.userCodedMessages = [];
             };
 
+            //class Message {
+            //	id: number;
+            //	label: number;
+            //	source: string;
+            //	isAmbiguous: boolean;
+            //	isSaved: boolean;
+            //	isExample: boolean;
+            //}
+
+            //class MessageDetail extends MessageData {
+            //	text: string;
+            //	tokens: string[];
+            //	filteredToFull: Map<number, number>;
+            //	fullToFiltered: Map<number, number>;
+            //	features: Feature[];
+            //	characters: string[];
+            //	charToToken: Map<number, number>;
+            //
+            //	// interaction
+            //  hoveredCharStart: number;
+            //  hoveredCharEnd: number;
+            //  clickStartTokenItem: Token;
+            //  selectedTokens: Token[];
+            //  selectedTokenIndices: Map<number, number>;
+            //}
+
             angular.extend(Message.prototype, {
-                load_message_details: function (source, message_id) {
+                getAllMessages: function(){
+                    var self = this;
+
+                    // TODO: this needs all messages, not just coded ones
+                    var apiUrl = djangoUrl.reverse('all_coded_messages');
+
+                    var request = {
+                        params: {
+                            stage: use_current_stage || undefined
+                        }
+                    };
+
+                    return $http.get(apiUrl, request)
+                        .success(function (data) {
+                            self.all_coded_messages = data.map(function(d){ return self.format_tweet_item(d);});
+
+                            // compute code distribution
+                            self.normalized_code_distribution = {};
+                            self.code_distribution = {};
+                            self.all_coded_messages.forEach(function (item){
+                               if (self.code_distribution[item.user_code.text] == undefined){
+                                   self.code_distribution[item.user_code.text] = 0;
+                               }
+
+                                self.code_distribution[item.user_code.text]++;
+                            });
+
+                            for (var key in self.code_distribution){
+                                self.normalized_code_distribution[key] = self.code_distribution[key] / self.all_coded_messages.length;
+                            }
+
+                            $rootScope.$broadcast("messages::load_all_coded_messages", data);
+                        });
+
+                },
+
+                // source: string
+                getCodedMessages: function(source){
+                    var self = this;
+                    var param = {};
+                    var source = source || "user";
+                    //if (source != "master")
+                    //    param.stage = "current";
+
+                    var apiUrl = djangoUrl.reverse('code_messages', param);
+
+
+                    var request = {
+                        params: {
+                            code: code.code_id,
+                            source: source
+                        }
+                    };
+
+                    return $http.get(apiUrl, request)
+                        .success(function (data) {
+                            self.coded_messages[source][code.code_text] = data.assignments.map(function(d){ return self.format_tweet_item(d);});
+                            $rootScope.$broadcast("messages::load_coded_messages", self.coded_messages);
+                        });
+
+
+                },
+
+                // message: Message
+                getMessageDetail: function (message) {
                     var self = this;
 
                     var request = {
@@ -156,6 +124,8 @@
                         });
 
                 },
+
+
                 format_tweet_item: function(messageData) {
                     var self = this;
 
@@ -471,260 +441,6 @@
             });
 
             return new Message();
-        }
-    ]);
-
-    //A service for code definition
-    module.factory('Aeonium.services.Code', [
-        '$http', 'djangoUrl', '$rootScope',
-        function codeFactory($http, djangoUrl, $rootScope) {
-
-            var Code = function () {
-                var self = this;
-                self.definitions_by_source = {
-                    master: [],
-                    user: [],
-                    partner: []
-                };
-                self.definitions_by_code = {};
-                self.codes = undefined;
-                self.pairwise_distribution = [];
-                self.pairwise_distribution_map = {};
-
-            };
-
-            angular.extend(Code.prototype, {
-                init_load: function(){
-                    var self = this;
-                    self.definitions_by_source = {};
-                    self.definitions_by_code = {};
-                    return self.load_definitions(["master", "user", "partner"]);
-                },
-
-                load_definitions: function (sources) {
-                    var self = this;
-                    var apiUrl = djangoUrl.reverse('definitions');
-
-                    var request = {
-                        params: {
-                            source: sources.join(" ")
-                        }
-                    };
-
-                    return $http.get(apiUrl, request)
-                        .success(function (def_sets) {
-                            def_sets.forEach(function(def_set){
-                                self.definitions_by_source[def_set.source] = def_set.definitions;
-                                def_set.definitions.forEach(function(def){
-                                    if (typeof(self.definitions_by_code[def.code_text]) === "undefined")
-                                        self.definitions_by_code[def.code_text] = {master: {text: ""},
-                                                                                   user: {text: ""},
-                                                                                   partner: {text: ""}};
-                                    self.definitions_by_code[def.code_text][def_set.source] = def;
-                                });
-                            });
-
-                            self.codes = self.definitions_by_source.master;
-
-                            $rootScope.$broadcast('definitions::updated', self.codes);
-                        });
-
-                },
-                update_definition: function(code){
-                    var self = this;
-                    var apiUrl = djangoUrl.reverse('definition', {code_id: code.code_id});
-
-
-
-                    var request = {
-                        text: self.definitions_by_code[code.code_text]["user"].text.trim()
-                    };
-
-
-                    return $http.put(apiUrl, request)
-                        .success(function (data) {
-                            console.log("Update definition for ", code);
-                        });
-
-
-                },
-                get_pairwise: function(use_current_stage){
-                    var self = this;
-                    var request = {
-                        params: {
-                            stage: use_current_stage  || undefined
-                        }
-                    };
-
-                    var apiUrl= djangoUrl.reverse('pairwise');
-                    return $http.get(apiUrl, request)
-                        .success(function (data) {
-                            self.pairwise_distribution = data;
-                            self.pairwise_distribution.forEach(function(pair){
-                                var key = pair.user_code + "_" + pair.partner_code;
-                                self.pairwise_distribution_map[key] = pair;
-                            });
-                        });
-                },
-                update_pairwise: function(original_code, new_code, partner_code){
-                    var self = this;
-                    var original_key = original_code + "_" + partner_code;
-                    var new_key = new_code + "_" + partner_code;
-
-                    if ( typeof(self.pairwise_distribution_map) !== "undefined") {
-                        self.pairwise_distribution_map[original_key].count--;
-                        self.pairwise_distribution_map[new_key].count++;
-                    }
-                }
-
-            });
-
-            return new Code();
-        }
-    ]);
-
-
-
-    //A service for progress
-    module.factory('Aeonium.services.Progress', [
-        '$http', 'djangoUrl', '$rootScope',
-        function progressFactory($http, djangoUrl, $rootScope) {
-
-
-
-            var Progress = function () {
-                var self = this;
-                self.current_stage_index = 0;
-                self.current_message_id = 0;
-                self.current_status = undefined;
-                self.current_message = undefined;
-
-                self.init_load(); // initialization
-            };
-
-            angular.extend(Progress.prototype, {
-                init_load: function(){
-                    var self = this;
-                    var apiUrl = djangoUrl.reverse('progress');
-
-                    //return $http.get(apiUrl, request)
-                    return $http.get(apiUrl)
-                        .success(function (data) {
-                            console.log(data);
-                            self.user = data.user;
-                            self.current_stage_index = data.current_stage_index;
-                            self.current_message_id = data.current_message_id;
-                            self.current_status = data.current_status;
-                        });
-                },
-                next_step: function(){
-                    var self = this;
-
-                    var apiUrl = djangoUrl.reverse('progress');
-
-
-                    return $http.post(apiUrl)
-                        .success(function (data) {
-                            self.current_stage_index = data.current_stage_index;
-                            self.current_message_id = data.current_message_id;
-                            self.current_status = data.current_status;
-                            self.is_finished = data.is_finished;
-                        })
-
-                }
-            });
-
-            return new Progress();
-        }
-
-    ]);
-
-
-    //A service for add history.
-    module.factory('Aeonium.services.ActionHistory', [
-        '$rootScope', '$http', '$interval', '$window', 'djangoUrl',
-        'Aeonium.services.Progress',
-        function actionHistoryFactory($rootScope, $http, $interval, $window, djangoUrl, Progress) {
-
-            var apiUrl = djangoUrl.reverse('action-history');
-
-            var ActionHistory = function () {
-                var self = this;
-                var second = 1000;
-                var submit_interval = 30;
-                self.queue = [];
-                var interval_timer = $interval(self.submit_records.bind(self), submit_interval * second);
-
-                /*
-                if ($window.location.search.indexOf('safe') != -1){
-                    window.onbeforeunload = function(){
-                        self.submit_records.call(self);
-                        return "Are you sure you want to leave the page?";
-                    };
-                }*/
-                window.onbeforeunload = function(){
-                    self.submit_records.call(self);
-                        return "Are you sure you want to leave the page?";
-                };
-
-                self.init();
-
-            };
-
-            angular.extend(ActionHistory.prototype, {
-                init: function(){
-                    var self = this;
-                    self.add_record('initialization:server-time', '', true);
-                    self.add_record('initialization:client-time', '');
-                    self.submit_records();
-                },
-                JSON_stringify: function(s, emit_unicode){
-                   var json = JSON.stringify(s);
-                   return emit_unicode ? json : json.replace(/[\u007f-\uffff]/g,
-                      function(c) {
-                        return '\\u'+('0000'+c.charCodeAt(0).toString(16)).slice(-4);
-                      }
-                   );
-                },
-                add_record: function (type, contents, use_server_time) {
-                    var self = this;
-                    if ( typeof(contents) !== typeof("string") ){
-                        contents = self.JSON_stringify(contents);
-                    }
-
-                    var record = {
-                        type: type,
-                        contents: contents,
-                        stage_index: Progress.current_stage_index || 0,
-                        status: Progress.current_status || 'N'
-                    };
-                    if (!use_server_time)
-                        record.created_at =  moment.utc().format('YYYY-MM-DD HH:mm:ss');
-                    console.log(record);
-                    self.queue.push(record);
-
-
-                },
-                submit_records: function(){
-                    var self = this;
-                    console.log(self.queue.length + " record(s) in the action history queue ... " );
-                    if (self.queue.length == 0) return;
-
-                    var request = self.queue;
-
-                    self.queue = [];
-
-                    return $http.post(apiUrl, request)
-                        .success(function (data) {
-                            console.log('save ' + data.length + ' record(s)');
-                        }).error(function (data) {
-                            // push the records back to queue
-                            self.queue.concat(request);
-                        });
-                }
-            });
-
-            return new ActionHistory();
         }
     ]);
 })();
