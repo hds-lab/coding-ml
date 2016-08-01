79e4ddc574d3d90541d030ca59c8bec64d6577ac
nanchenchen <nanchen.chen@gmail.com>
2016-08-01 07:12:46 -0700

    add to cache & broadcast data

--- msgvis/static/aeonium/models/MessageList.js
+++ msgvis/static/aeonium/models/MessageList.js
@@ -17,31 +17,13 @@
                 self.listCache.ambiguity = {};
             };
 
-            //class Message {
-            //	id: number;
-            //	label: number;
-            //	source: string;
-            //	isAmbiguous: boolean;
-            //	isSaved: boolean;
-            //	isExample: boolean;
-            //}
+            // class MessageList {
+            //     listCache: dict;
+            //     listCache.time: dict;
+            //     listCache.last_updated: dict;
+            //     listCache.ambiguity: dict;
+            // }
 
-            //class MessageDetail extends MessageData {
-            //	text: string;
-            //	tokens: string[];
-            //	filteredToFull: Map<number, number>;
-            //	fullToFiltered: Map<number, number>;
-            //	featureHighlights: FeatureHighlight[];
-            //	characters: string[];
-            //	charToToken: Map<number, number>;
-            //
-            //	// interaction
-            //  hoveredCharStart: number;
-            //  hoveredCharEnd: number;
-            //  clickStartTokenItem: Token;
-            //  selectedTokens: Token[];
-            //  selectedTokenIndices: Map<number, number>;
-            //}
 
             angular.extend(MessageList.prototype, {
                 getList: function(order_by, page){
@@ -55,87 +37,20 @@
                         }
                     };
                     if (self.listCache[order_by].hasOwnProperty(page)){
-                        // TODO return and trigger broadcast event
-
-
+                        var data = self.listCache[order_by][page];
+                        $rootScope.$broadcast("list::getList", data);
+                        return false;
                     }
-
 
                     return $http.get(apiUrl, request)
                         .success(function (data) {
-                            // TODO update cache
 
+                            self.listCache[order_by][page] = data;
                             $rootScope.$broadcast("list::getList", data);
                         });
 
                 },
 
-                // code: Code
-                getCodedMessages: function(code){
-                    var self = this;
-                    var param = {};
-                    var source = "user";
-                    //if (source != "master")
-                    //    param.stage = "current";
-
-                    var apiUrl = djangoUrl.reverse('code_messages', param);
-
-
-                    var request = {
-                        params: {
-                            code: code.codeId,
-                            source: source
-                        }
-                    };
-
-                    return $http.get(apiUrl, request)
-                        .success(function (data) {
-                            self.userCodedMessages[code.name] = data.assignments.map(function(d){ return Utils.extractMessageDetail(d);});
-                            $rootScope.$broadcast("messages::userCodedMessages::" + code.name, self.userCodedMessages);
-                        });
-
-                },
-
-                // message: Message
-                getMessageDetail: function (message) {
-                    var self = this;
-
-                    var request = {
-                        message_id: message.id,
-                        params: {
-                            source: "user"
-                        }
-                    };
-
-                    var apiUrl = djangoUrl.reverse('vector', request);
-
-                    return $http.get(apiUrl, request)
-                        .success(function (data) {
-                            var messageDetail = Utils.extractMessageDetail(data);
-                            $rootScope.$broadcast("messages::messageDetail::" + message.id, messageDetail);
-                        });
-
-                },
-
-                // message: Message
-                submitLabel: function (message) {
-                    var self = this;
-                    var apiUrl = djangoUrl.reverse('assignment', {message_id: message.id});
-
-                    var request = {
-                        code: message.label,
-                        is_example: message.isExample,
-                        is_ambiguous: message.isAmbiguous,
-                        is_saved: message.isSaved
-                    };
-
-
-                    return $http.post(apiUrl, request)
-                        .success(function (data) {
-                            $rootScope.$broadcast("messages::submitLabel::" + message.id, message);
-                        });
-
-                },
             });
 
             return new Message();
