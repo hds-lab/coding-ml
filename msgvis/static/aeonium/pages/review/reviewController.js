a07d2f3c74ea2fd4544fc662913e9441c75acbc1
Jina Suh <jasmart+github@gmail.com>
2017-01-26 16:40:40 -0800

    Move review page to its own folder

--- msgvis/static/aeonium/pages/review/reviewController.js
+++ msgvis/static/aeonium/pages/review/reviewController.js
@@ -122,7 +122,7 @@
             scope: {
             },
             controller: ReviewController,
-            templateUrl: 'static/aeonium/pages/review.html'
+            templateUrl: 'static/aeonium/pages/review/review.html'
         }
     });
 })();
